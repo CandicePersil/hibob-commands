42b6ff30a88757f9332347dceeed9bd23f476bbc
Candice PERSIL <candice.persil@kraken.tech>
2026-02-16 15:55:32 +0100

    Feat: Create csv extractor to generate csv from HiBob search results

---
+++ src/extractor/csv_dump.py
@@ -0,0 +1,27 @@
+import csv
+
+class CsvWriter:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+        self.field_names = [
+            "email",
+            "first_name",
+            "last_name",
+            "start_date",
+            "end_date",
+            "contract_type",
+            "date_of_birth",
+            "phone_number",
+            "professional_category",
+            "subsidy_level",
+            "area",
+        ]
+
+    def write_to_file(self, lines: list[dict[str, str | None]]) -> None:
+        if not lines:
+            return
+
+        with open(self.file_path, "w", newline="") as csv_file:
+            writer = csv.DictWriter(csv_file, delimiter=",", fieldnames=self.field_names)
+            writer.writeheader()
+            writer.writerows(lines)
